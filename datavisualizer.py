ccad353d5df996d43d8704996db87b78719338e3
JAB1305 <jabenning13@gmail.com>
2022-05-07 11:47:15 +0200

    Implement basic data visualization functionality

--- datavisualizer.py
+++ datavisualizer.py
@@ -7,31 +7,57 @@ def press(button):
     if button == "Cancel":
         app.stop()
     else:
-        data = app.getTextArea("Data")
+        try:
+            data = app.getTextArea("Data")
+            data = data.replace("[", "")
+            data = data.replace("]", "")
+            data = data.replace(" ", "")
+            data = data.split(",")
+            data = [int(x) for x in data]
+
+            summed_data = []
+            c1 = app.getCanvas("c1")
+
+            num_hash = 0
+            for i in range(len(data)):
+                num_hash += data[i]
+                if i % 2 != 0:
+                    summed_data.append(num_hash)
+                    num_hash = 0
+
+            for i in range(1, int(len(summed_data) / 4) + 1):
+                base_index = i * 4 - 1
+                offset = 1000
+
+                x_center = offset + summed_data[base_index - 3]
+                y_center = summed_data[base_index - 2]
+                width = summed_data[base_index - 1]
+                height = summed_data[base_index]
+
+                c1.create_rectangle(
+                    x_center - int(width / 2),
+                    y_center - int(height / 2),
+                    x_center + int(width / 2),
+                    y_center + int(height / 2)
+                )
+        except:
+            app.errorBox("Fehler", "Daten invalide. Kopf entfernen")
 
 
-
-# create a GUI variable called app
 app = gui("Login Window", "400x200")
 app.setBg("white")
-app.setFont(18)
 
-# add & configure widgets - widgets get a name, to help referencing them later
-app.addLabel("title", "Data visualization")
+app.addLabel("title", "Data visualization | Remove head before usage")
 app.setLabelBg("title", "white")
 app.setLabelFg("title", "black")
 
 app.addTextArea("Data")
 app.setTextAreaHeight("Data", 10)
-
-# link the buttons to the function called press
 app.addButtons(["Submit", "Cancel"], press)
 
-
 canvas = app.addCanvas("c1")
+# outside box
 canvas.create_rectangle(1000, 0, 1316, 208)
 
 app.setSize("Fullscreen")
-
-# start the GUI
 app.go()
