fb4a7c9e08a2863521ce97c9408400f74cd55530
JAB1305 <jabenning13@gmail.com>
2022-09-12 20:13:43 +0200

    Update blub.py

--- blub.py
+++ blub.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+from pybricks.iodevices import I2CDevice
 from smbus import SMBus
 from ev3dev2.display import *
 from ev3dev2.sensor import *
@@ -18,15 +18,16 @@ def only_contains_one_element(data):
     return len(set(data)) == 1
 
 def readBlocks(current_direction):
+    device = I2CDevice(INPUT_2, c.CAMERA_ADDRESS)
     data = [174, 193, 32, 2, 255, 255]
-    c.BUS.write_i2c_block_data(c.CAMERA_ADDRESS, 0, data)
+    device.write(None, data)
     # Read first block
     data = ""
-    block = c.BUS.read_i2c_block_data(c.CAMERA_ADDRESS, 0, 6 + 14)
+    block = device.read(0, 6 + 14)
     if not only_contains_one_element(block[7:]):
         data += str(block)
     while True:
-        block2 = c.BUS.read_i2c_block_data(c.CAMERA_ADDRESS, 0, 14)
+        block2 = device.read(0, 14)
         if only_contains_one_element(block2):
             break
         data += "|\n" + str(block2)
